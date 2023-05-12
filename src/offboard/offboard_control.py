f996ab4c514cdaadfe4df6b1396443c4f5c24437
JU-SUK <82742016+JU-SUK@users.noreply.github.com>
2023-05-12 14:47:20 +0900

    Create offboard_control.py

---
+++ src/offboard/offboard_control.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+from __future__ import division
+import rospy
+import math
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Imu
+from mavros_msgs.msg import State,Altitude, HomePosition, ExtendedState, PositionTarget
+from mavros_msgs.srv import CommandBool,SetMode,CommandTOL, ParamGet, StreamRateRequest,StreamRate
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64, String,Bool
+from geometry_msgs.msg import Vector3, Point
+from tf.transformations import euler_from_quaternion
